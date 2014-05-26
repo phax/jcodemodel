3b24b6c18d8ae8cdbe834bb7e907b553a205485b
Victor Nazarov <asviraspossible@gmail.com>
2014-05-26 15:42:37 +0400

    Refactor reference collection process
    Refactor reference collection process of JFormatter.
    Rename predicate-methods to more understandable and correct names.
    Rename `collisions` to `isAmbiguousIn`.
    Rename `supressImport` to `isImplicitlyImported`
    Introduce new `isImportable` predicate.

--- src/test/java/com/helger/jcodemodel/tests/InnerClassTest.java
+++ src/test/java/com/helger/jcodemodel/tests/InnerClassTest.java
@@ -16,16 +16,28 @@ public class InnerClassTest
   {
     final JCodeModel codeModel = new JCodeModel ();
     final JDefinedClass aClass = codeModel._class ("org.test.DaTestClass");
-    final JDefinedClass daInner = aClass._class ("Inner");
+    final JDefinedClass daInner1 = aClass._class ("Inner");
+    final JDefinedClass daInnerInner = daInner1._class ("InnerInner");
+    final JDefinedClass daInner2 = aClass._class ("DaTestClassInner");
+    final JDefinedClass daInner2Inner = daInner2._class ("Inner2");
 
-    assertEquals ("Inner", daInner.name ());
-    assertEquals ("org.test.DaTestClass.Inner", daInner.fullName ());
-    assertEquals ("org.test.DaTestClass$Inner", daInner.binaryName ());
+    assertEquals ("Inner", daInner1.name ());
+    assertEquals ("org.test.DaTestClass.Inner", daInner1.fullName ());
+    assertEquals ("org.test.DaTestClass$Inner", daInner1.binaryName ());
 
-    aClass.method (JMod.PUBLIC, daInner, "getInner");
+    assertEquals ("InnerInner", daInnerInner.name ());
+    assertEquals ("org.test.DaTestClass.Inner.InnerInner", daInnerInner.fullName ());
+    assertEquals ("org.test.DaTestClass$Inner$InnerInner", daInnerInner.binaryName ());
+
+    aClass.method (JMod.PUBLIC, daInner1, "getInner");
+    aClass.method (JMod.PUBLIC, daInnerInner, "getInnerInner");
+    aClass.method (JMod.PUBLIC, daInner2, "getInner2");
+    aClass.method (JMod.PUBLIC, daInner2Inner, "getInner2Inner");
 
     final JDefinedClass otherClass = codeModel._class ("org.test.OtherClass");
-    otherClass.method (JMod.PUBLIC, daInner, "getInner");
+    otherClass.method (JMod.PUBLIC, daInner1, "getInner");
+    otherClass.method (JMod.PUBLIC, daInnerInner, "getInnerInner");
+    otherClass.method (JMod.PUBLIC, daInner2Inner, "getInner2Inner");
     otherClass.method (JMod.PUBLIC, aClass, "getOuter");
 
     codeModel.build (new SingleStreamCodeWriter (System.out));
