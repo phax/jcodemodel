a1100c2b3aeaf3569966911f468e9542a68ce0de
phax <philip@helger.com>
2015-09-13 22:16:53 +0200

    Made constructor public

--- src/main/java/com/helger/jcodemodel/JEnumConstant.java
+++ src/main/java/com/helger/jcodemodel/JEnumConstant.java
@@ -84,7 +84,7 @@ public class JEnumConstant extends AbstractJExpressionImpl implements IJDeclarat
    */
   private List <IJExpression> _args;
 
-  protected JEnumConstant (@Nonnull final JDefinedClass type, @Nonnull final String name)
+  public JEnumConstant (@Nonnull final JDefinedClass type, @Nonnull final String name)
   {
     if (type == null)
       throw new NullPointerException ("type");
