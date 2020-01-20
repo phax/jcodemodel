bf298444efbced8c3923da009649865759d4af51
glelouet <guillaume.lelouet@gmail.com>
2020-01-20 22:51:17 +0100

    javadoc is good

--- src/main/java/com/helger/jcodemodel/exceptions/JCaseSensitivityChangeException.java
+++ src/main/java/com/helger/jcodemodel/exceptions/JCaseSensitivityChangeException.java
@@ -1,7 +1,17 @@
 package com.helger.jcodemodel.exceptions;
 
+import com.helger.jcodemodel.JCodeModel;
 import com.helger.jcodemodel.JCodeModelException;
+import com.helger.jcodemodel.util.IFileSystemConvention;
 
+/**
+ * Exception thrown when trying to replace the existing
+ * {@link IFileSystemConvention} of a {@link JCodeModel} by another one with a
+ * different case sensitivity
+ *
+ * @author glelouet
+ *
+ */
 public class JCaseSensitivityChangeException extends JCodeModelException {
 
   /**
