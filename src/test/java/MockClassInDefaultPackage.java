3103972a2947fd41bb27bd2cb2aa863ed853f1b0
Philip Helger <philip@helger.com>
2021-01-01 13:34:22 +0100

    Updated to 2021

--- src/test/java/MockClassInDefaultPackage.java
+++ src/test/java/MockClassInDefaultPackage.java
@@ -2,7 +2,7 @@
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
  * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
- * Portions Copyright 2013-2020 Philip Helger + contributors
+ * Portions Copyright 2013-2021 Philip Helger + contributors
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -38,7 +38,6 @@
  * only if the new code is made subject to such option by the copyright
  * holder.
  */
-
 /**
  * Mock class inside the default package
  *
