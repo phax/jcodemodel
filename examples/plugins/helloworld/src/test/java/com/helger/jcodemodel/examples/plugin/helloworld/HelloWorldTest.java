b6d148c53bcf00ae18e9e828fbb2bb3ec73e1302
glelouet <n@mo>
2025-08-22 18:03:23 +0200

    forgot to test the first hello file

--- examples/plugins/helloworld/src/test/java/com/helger/jcodemodel/examples/plugin/helloworld/HelloWorldTest.java
+++ examples/plugins/helloworld/src/test/java/com/helger/jcodemodel/examples/plugin/helloworld/HelloWorldTest.java
@@ -14,6 +14,9 @@ public class HelloWorldTest {
   public void testValue() {
     Hello test = new Hello();
     Assert.assertEquals("world", test.value);
+    File filejava = new File("src/generated/java/com/helger/tests/helloworld/Hello.java");
+    Assert.assertTrue("missing file " + filejava.getAbsolutePath(), filejava.isFile());
+
     Hello2 test2 = new Hello2();
     Assert.assertEquals("world2", test2.value);
     File filejava2 = new File("src/generated/java2/com/helger/tests/helloworld/Hello2.java");
