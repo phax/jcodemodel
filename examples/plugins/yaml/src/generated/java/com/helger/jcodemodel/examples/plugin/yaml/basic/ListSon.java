fca3dba40fbae514eafacd79781040cd732abc08
glelouet <n@mo>
2025-09-11 13:08:26 +0200

    minor fix

--- examples/plugins/yaml/src/generated/java/com/helger/jcodemodel/examples/plugin/yaml/basic/ListSon.java
+++ examples/plugins/yaml/src/generated/java/com/helger/jcodemodel/examples/plugin/yaml/basic/ListSon.java
@@ -1,8 +1,10 @@
 package com.helger.jcodemodel.examples.plugin.yaml.basic;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListSon
     extends ArrayList<ArrayList<Object>>
 {
+    public List<List<Object>> listf;
 }
