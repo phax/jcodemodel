6e2b0fa6b40413d2112927df65e93c64cb13455c
glelouet <n@mo>
2026-07-18 17:49:17 +0200

    added formatter settings

--- jcodemodel/src/main/java/com/helger/jcodemodel/writer/FormatterSettings.java
+++ jcodemodel/src/main/java/com/helger/jcodemodel/writer/FormatterSettings.java
@@ -4,8 +4,62 @@ import java.util.function.Consumer;
 
 import com.helger.jcodemodel.writer.settings.Indent;
 import com.helger.jcodemodel.writer.settings.Wrap;
+import com.helger.jcodemodel.writer.settings.Wrap.ListWrapping.EListWrapStrategy;
+import com.helger.jcodemodel.writer.settings.Wrap.WordWrapping.EWordWrapStrategy;
 
 public class FormatterSettings {
+
+  //
+  // help configs
+  //
+
+  public static final Consumer<FormatterSettings> CONF_PHELGER = settings -> {
+    settings.indent
+        .useSpaces(4)
+        .tabSize(4);
+    settings.wrap.method.args
+        .condition(EListWrapStrategy.REQUIRED);
+    settings.wrap.method.bracket
+        .condition(EWordWrapStrategy.ALWAYS);
+  };
+
+  public static FormatterSettings phelger() {
+    return new FormatterSettings().configure(CONF_PHELGER);
+  }
+
+  public static final Consumer<FormatterSettings> CONF_GLELOUET = settings -> {
+    settings.indent
+        .useTabs(1)
+        .tabSize(2);
+    settings.wrap.catchClause.types
+        .condition(EListWrapStrategy.BINARY)
+        .indent(2);
+    settings.wrap.forLoop.init
+        .condition(EListWrapStrategy.BINARY)
+        .indent(2);
+    settings.wrap.method.args
+        .condition(EListWrapStrategy.BINARY)
+        .indent(2);
+    settings.wrap.method.bracket
+        .condition(EWordWrapStrategy.NEVER);
+    settings.wrap.method.params
+        .condition(EListWrapStrategy.BINARY)
+        .indent(2);
+    settings.wrap.variables.block
+        .condition(EListWrapStrategy.BINARY)
+        .indent(2);
+    settings.wrap.variables.field
+        .condition(EListWrapStrategy.BINARY)
+        .indent(2);
+  };
+
+  public static FormatterSettings glelouet() {
+    return new FormatterSettings().configure(CONF_PHELGER);
+  }
+
+  //
+  //
+  //
 
   public final Indent indent = new Indent();
 
