7f070897017659f27d389e160a756d6061f0151c
glelouet <n@mo>
2026-07-22 14:22:47 +0200

    JArray.element to list

--- jcodemodel/src/main/java/com/helger/jcodemodel/expression/JArrayInit.java
+++ jcodemodel/src/main/java/com/helger/jcodemodel/expression/JArrayInit.java
@@ -12,23 +12,23 @@ import com.helger.jcodemodel.JExpr;
 public class JArrayInit implements IVariableInitializer {
 
   @NonNull
-  private final IVariableInitializer[] elements;
+  private final List <IVariableInitializer> elements;
 
   public JArrayInit(IVariableInitializer... elements) {
     this.elements = convertElements(elements);
   }
 
-  static IVariableInitializer[] convertElements(IVariableInitializer[] elements) {
-    if (elements == null) {
-      return new IVariableInitializer[] {};
-    }
-    return Stream.of(elements).map(e -> e != null ? e : JExpr._null()).toArray(IVariableInitializer[]::new);
+  static List <IVariableInitializer> convertElements (IVariableInitializer[] elements)
+  {
+    if (elements == null)
+      return List.of ();
+    return Stream.of (elements).map (e -> e != null ? e : JExpr._null ()).toList ();
   }
 
   @Override
   public void generate(@NonNull IJFormatter f) {
     f.print('{');
-    f.generable(List.of(elements), ",", f.settings().wrap.variables.array);
+    f.generable (elements, ",", f.settings ().wrap.variables.array);
     f.print('}');
   }
 
