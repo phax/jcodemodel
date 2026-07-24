c954fca4cf4a92b651a921237d9f5a7a9681a6e7
glelouet <n@mo>
2026-07-24 15:35:55 +0200

    added generated int format files

---
+++ jcodemodeltests/src/generated/javatest/com/helger/jcodemodel/tests/intformat/FormatIntWithSeparatorEvery0Size1.java
@@ -0,0 +1,33 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.helger.jcodemodel.tests.intformat;
+
+import javax.annotation.processing.Generated;
+
+@Generated("com.helger.jcodemodel.JCodeModel")
+public class FormatIntWithSeparatorEvery0Size1 {
+    public static final int i32b = 0b100000;
+    public static final int i32d = 32;
+    public static final int i32h = 0x20;
+    public static final int i32o = 040;
+    public static final int i1Mb = 0b100000000000000000000;
+    public static final int i1Md = 1048576;
+    public static final int i1Mh = 0x100000;
+    public static final int i1Mo = 04000000;
+    public static final int iNeg1kb = -0b10000000000;
+    public static final int iNeg1kd = -1024;
+    public static final int iNeg1kh = -0x400;
+    public static final int iNeg1ko = -02000;
+}
---
+++ jcodemodeltests/src/generated/javatest/com/helger/jcodemodel/tests/intformat/FormatIntWithSeparatorEvery2Size2.java
@@ -0,0 +1,33 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.helger.jcodemodel.tests.intformat;
+
+import javax.annotation.processing.Generated;
+
+@Generated("com.helger.jcodemodel.JCodeModel")
+public class FormatIntWithSeparatorEvery2Size2 {
+    public static final int i32b = 0b10__00__00;
+    public static final int i32d = 32;
+    public static final int i32h = 0x20;
+    public static final int i32o = 040;
+    public static final int i1Mb = 0b1__00__00__00__00__00__00__00__00__00__00;
+    public static final int i1Md = 1__04__85__76;
+    public static final int i1Mh = 0x10__00__00;
+    public static final int i1Mo = 04__00__00__00;
+    public static final int iNeg1kb = -0b1__00__00__00__00__00;
+    public static final int iNeg1kd = -10__24;
+    public static final int iNeg1kh = -0x4__00;
+    public static final int iNeg1ko = -020__00;
+}
