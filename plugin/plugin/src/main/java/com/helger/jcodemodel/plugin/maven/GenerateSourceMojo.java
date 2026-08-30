5f18092d2c19ab03e65b929eb951eefba435299c
glelouet <n@mo>
2026-08-30 14:56:01 +0200

    wrong exception, went too fast

--- plugin/plugin/src/main/java/com/helger/jcodemodel/plugin/maven/GenerateSourceMojo.java
+++ plugin/plugin/src/main/java/com/helger/jcodemodel/plugin/maven/GenerateSourceMojo.java
@@ -283,7 +283,7 @@ public class GenerateSourceMojo extends AbstractMojo
       catch (FileNotFoundException e)
       {
         // should never happens since isFile checks for existence
-        throw new IllegalArgumentException (e);
+        throw new IllegalStateException (e);
       }
     }
     else
