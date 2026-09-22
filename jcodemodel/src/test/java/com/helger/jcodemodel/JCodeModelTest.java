cea5f7e80f726fd50a817d012c045fdc85d8c336
glelouet <3405335+glelouet@users.noreply.github.com>
2026-09-22 17:58:24 +0200

    format tabs

--- jcodemodel/src/test/java/com/helger/jcodemodel/JCodeModelTest.java
+++ jcodemodel/src/test/java/com/helger/jcodemodel/JCodeModelTest.java
@@ -247,25 +247,26 @@ public final class JCodeModelTest
     final JCodeModel jcm = JCodeModel.createUnified ();
 
     {
-    // void Map::putAll(Map<? extends K, ? extends V> m);
-    Method mapPutAll = Map.class.getMethod ("putAll", Map.class);
-    Type firstParam = mapPutAll.getParameters ()[0].getParameterizedType ();
-    Assert.assertEquals ("java.util.Map<? extends K, ? extends V>",
-                         CodeModelTestsHelper.generate (jcm.ref (firstParam)));
-  }
-  {
-    // Set<Map.Entry<K, V>> Map::entrySet();
-    Method mapEntrySet = Map.class.getMethod ("entrySet");
-    Type retType = mapEntrySet.getGenericReturnType ();
-    Assert.assertEquals ("java.util.Set<java.util.Map.Entry<K, V>>", CodeModelTestsHelper.generate (jcm.ref (retType)));
-  }
-  {
-    // Set<Map.Entry<K, V>> Map::entrySet();
-    Method listToArray = List.class.getMethod ("toArray", Object [].class);
-    Type retType = listToArray.getGenericReturnType ();
-    Assert.assertEquals ("T[]", CodeModelTestsHelper.generate (jcm.ref (retType)));
-    Type firstParam = listToArray.getParameters ()[0].getParameterizedType ();
-    Assert.assertEquals ("T[]", CodeModelTestsHelper.generate (jcm.ref (firstParam)));
-  }
+      // void Map::putAll(Map<? extends K, ? extends V> m);
+      Method mapPutAll = Map.class.getMethod ("putAll", Map.class);
+      Type firstParam = mapPutAll.getParameters ()[0].getParameterizedType ();
+      Assert.assertEquals ("java.util.Map<? extends K, ? extends V>",
+                           CodeModelTestsHelper.generate (jcm.ref (firstParam)));
+    }
+    {
+      // Set<Map.Entry<K, V>> Map::entrySet();
+      Method mapEntrySet = Map.class.getMethod ("entrySet");
+      Type retType = mapEntrySet.getGenericReturnType ();
+      Assert.assertEquals ("java.util.Set<java.util.Map.Entry<K, V>>",
+                           CodeModelTestsHelper.generate (jcm.ref (retType)));
+    }
+    {
+      // Set<Map.Entry<K, V>> Map::entrySet();
+      Method listToArray = List.class.getMethod ("toArray", Object [].class);
+      Type retType = listToArray.getGenericReturnType ();
+      Assert.assertEquals ("T[]", CodeModelTestsHelper.generate (jcm.ref (retType)));
+      Type firstParam = listToArray.getParameters ()[0].getParameterizedType ();
+      Assert.assertEquals ("T[]", CodeModelTestsHelper.generate (jcm.ref (firstParam)));
+    }
   }
 }
